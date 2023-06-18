431eafbfe35847cbc405f89e992034d6538bdce3
Михаил Соколов <sokol.m00@gmail.com>
2023-06-19 03:17:29 +0700

    some commit

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
     <h1>REGISTER</h1>
+    <input type="email"
+    name="email"
+    v-model="email"
+    placeholder="E-mail"/>
+    <br>
+    <input type="password"
+    name="pwd"
+    v-model="password"
+    placeholder="password"/>
+    <br>
+    <button @click="register">Регистрация!</button>
   </div>
 </template>
 
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
 export default {
-  name: 'HelloWorld',
+  // name: 'Register',
   data () {
     return {
-      
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      // console.log('register button was clicked')
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
     }
   }
 }
--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -1,13 +1,3 @@
 import Api from '@/services/Api'
 
-export default {
-    register (credentials){
-        return Api().post('register', credentials)
-    }
-}
-
-
-// AuthenticationService.Api.register({
-//     email: 'testing@gmail.com',
-//     password: '123456'
-// })
+export default { register (credentials) { return Api().post('register', credentials) } }
