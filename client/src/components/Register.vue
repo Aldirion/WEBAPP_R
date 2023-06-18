d8abdf0f4ad9f575d1e3e517826d5edfea1790a9
Михаил Соколов <sokol.m00@gmail.com>
2023-06-19 04:33:22 +0700

    Registration page was added

---
+++ client/src/components/Register.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Регистрация</h1>
+    <input type="email"
+    name="email"
+    placeholder="E-mail"
+    v-model="email">
+    <br>
+    <input type="password"
+    name="password"
+    placeholder="Password"
+    v-model="password">
+    <br>
+    <button @click="register">Зарегистрироваться</button>
+
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'Register',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+      // console.log('register button was clicked', this.email, this.password)
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => { res.send({ message: 'hello, vue!' }) })
-
-app.listen(process.env.PORT || 8082)
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello, vue!'
+  })
+})
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello, ${req.body.email}, user was registered`
+  })
+})
+app.listen(process.env.PORT || 8081)
